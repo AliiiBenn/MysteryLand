340dcce71beb3d49c3041ddd4796d579fd467caf
DahmaneYanis <dahmane.yanis.2004@gmail.com>
2022-03-10 23:35:18 +0100

    Ajout images projet

--- Maps/Tilesets/Interiors.tsx
+++ Maps/Tilesets/Interiors.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.8" tiledversion="1.8.1" name="Interiors" tilewidth="16" tileheight="16" tilecount="17024" columns="16">
- <image source="F:/Programmation/Assets/Images/Images Projet Yanis Jules/Images Projet Yanis Jules/Modern_Interiors/Modern_Interiors/1_Interiors/16x16/Interiors_16x16.png" trans="003039" width="256" height="17024"/>
+ <image source="../../img/Interiors_16x16.png" trans="003039" width="256" height="17024"/>
 </tileset>
