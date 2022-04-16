8e8c1607a24c6cd9682657301040b5db8e01e7e3
d_yanis <dahmane.yanis.2004@gmail.com>
2022-04-16 11:04:18 +0200

    Base de la mise en place du son

--- src/widgets/sound.py
+++ src/widgets/sound.py
@@ -1,5 +1,17 @@
 import pygame as py
 
-class Soundnpc:
+class Sound:
+    def __init__(self):
+        pass
+
+class SoundPlayer(Sound):
+    def __init__(self):
+        pass
+
+class SoundNPC(Sound):
+    def __init__(self, nomNpc):
+        self.name = nomNpc
+
+class SoundGlobal(Sound):
     def __init__(self):
         pass
